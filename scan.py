aed93e863df5614b5419ed132ab99311af04860e
denys <dr.dan79@gmail.com>
2016-12-15 15:13:43 +0200

    restructuring

--- scan.py
+++ scan.py
@@ -11,17 +11,8 @@ from smtplib import SMTP
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-parser = ArgumentParser(description="ClamAV Scanner. Example: ./scan.py '/root' 'billgates@microsoft.com' ")
-parser.add_argument('dir', type=str, help='Directory to scan')
-parser.add_argument('sendto', type=str, help='Report recipient address')
 
-_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-_dir = parser.parse_args().dir
-_recipient = parser.parse_args().sendto
-_host = node()
-
-
-def send_mail(html, recipient):
+def send_mail(html, recipient, subject):
     user = '<hidden>'
     password = '<hidden>'
     header_from = '<hidden>'
@@ -29,7 +20,7 @@ def send_mail(html, recipient):
     msg = MIMEMultipart()
     msg['From'] = user
     msg['To'] = recipient
-    msg['Subject'] = "[{0}] ClamAV: scanned {1} on {2}".format(_now, _dir, _host)
+    msg['Subject'] = subject
 
     msg.attach(MIMEText(html, 'html'))
 
@@ -41,26 +32,48 @@ def send_mail(html, recipient):
     client.quit()
 
 
-data = []
+def parse_clamav(parsed_args_p):
+    data = []
 
-try:
-    for line in run(['clamscan', '-r', '-i', '--no-summary', '{}'.format(_dir)], stdout=PIPE).stdout.decode(
-            'utf-8').split('\n'):
-        if line:
-            data.append(''.join(line.split()[0:2]).split(':'))
-except OSError:
-    print("Please check ClamAV is installed or present in PATH")
-    exit(1)
+    try:
+        for line in run(['clamscan', '-r', '-i', '--no-summary', '{}'.format(parsed_args_p.dir)],
+                        stdout=PIPE).stdout.decode('utf-8').split('\n'):
+            if line:
+                data.append(''.join(line.split()[0:2]).split(':'))
+    except OSError:
+        print("Please check ClamAV is installed or present in PATH")
+        exit(1)
 
-loader = FileSystemLoader(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
-env = Environment(loader=loader, trim_blocks=True, lstrip_blocks=True)
-template = env.get_template('report.tmpl')
+    if not data:
+        sys.exit(0)
 
-if data:
+    return data
+
+
+def form_template(data, parsed_args_t, now_t, host_t):
+
+    loader = FileSystemLoader(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
+    env = Environment(loader=loader, trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template('report.tmpl')
+
     rendered_html = (template.render(data={
         'data': data,
-        'time': _now,
-        'dir': _dir,
-        'host': _host
+        'time': now_t,
+        'dir': parsed_args_t.dir,
+        'host': host_t
     }))
-    send_mail(rendered_html, recipient=_recipient)
+
+    return rendered_html
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="ClamAV Scanner. Example: ./scan.py '/root' 'billgates@microsoft.com' ")
+    parser.add_argument('dir', type=str, help='Directory to scan')
+    parser.add_argument('sendto', type=str, help='Report recipient address')
+    parsed_args = parser.parse_args()
+
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    host = node()
+
+    send_mail(form_template(parse_clamav(parsed_args), parsed_args, now, host), recipient=parsed_args.sendto,
+              subject="[{0}] ClamAV: scanned {1} on {2}".format(now, parsed_args.dir, host))
